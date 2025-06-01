d71fb7e6ad897065ea5aee826df7d60f48200cca
JoaquimCruz <joaquimcezar930@gmail.com>
2025-06-01 18:00:30 -0300

    adding files to gitignore

--- rest-with-spring-boot/src/main/java/br/com/joaquim/data/dto/v1/PersonDTO.java
+++ rest-with-spring-boot/src/main/java/br/com/joaquim/data/dto/v1/PersonDTO.java
@@ -14,7 +14,6 @@ import java.util.Objects;
 public class PersonDTO extends RepresentationModel<PersonDTO> implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     // @JsonProperty("id_usuario")
     private Long id;
 
